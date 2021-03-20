382d9f1e0d31dd283f834a6e48a08de16e833a7f
Mutte Chen <mutte109@gmail.com>
2021-03-20 15:18:08 -0700

    Added public constructor class

--- src/IAT455CourseProject.java
+++ src/IAT455CourseProject.java
@@ -1,6 +1,20 @@
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class IAT455CourseProject {
+public class IAT455CourseProject extends Frame {
 
+	public IAT455CourseProject() {
+		
+		this.addWindowListener(
+				new WindowAdapter(){//anonymous class definition
+					public void windowClosing(WindowEvent e){
+						System.exit(0);//terminate the program
+					}//end windowClosing()
+				}//end WindowAdapter
+				);//end addWindowListener
+	}// end constructor
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
