383b7d12dd451ce55de9dfb1169490165d813b79
Nikolas <abraham.nikolas@gmail.com>
2021-03-20 15:24:55 -0700

    combine images method

--- src/IAT455CourseProject.java
+++ src/IAT455CourseProject.java
@@ -1,7 +1,9 @@
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 
 public class IAT455CourseProject extends Frame {
 
@@ -15,6 +17,21 @@ public class IAT455CourseProject extends Frame {
 				}//end WindowAdapter
 				);//end addWindowListener
 	}// end constructor
+	public BufferedImage combineImages(BufferedImage src1, BufferedImage src2)
+    {
+        BufferedImage result = new BufferedImage(src1.getWidth(),src1.getHeight(),src1.getType());
+
+        for(int i=0; i <result.getWidth();i++){
+            for (int j=0; j< result.getHeight();j++){
+                int rgb1 = src1.getRGB(i,j);
+                int rgb2 = src2.getRGB(i,j);
+                int newR = 0,newG = 0,newB = 0;
+
+                result.setRGB(i,j, new Color (newR,newG,newB).getRGB());
+            }
+        }
+        return result;
+    }
 	
 	public void paint(Graphics g){
 		
